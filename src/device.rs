372a8041255a34d48cc42e2a4f04ecaaf94f4fca
Josh Kunz <josh@kunz.xyz>
2021-01-10 23:32:23 -0800

    Export sensor metrics via the prometheus interface.

--- src/device.rs
+++ src/device.rs
@@ -1,6 +1,7 @@
 use crate::wire;
 use serialport;
 use std::convert::TryFrom;
+use std::io;
 use std::io::{Read, Write};
 use std::result;
 use std::time;
@@ -8,13 +9,44 @@ use std::time;
 #[derive(Debug, PartialEq)]
 pub struct Error(String);
 
-impl<T: ToString> From<T> for Error {
-    fn from(v: T) -> Error {
-        Error(v.to_string())
+impl ToString for Error {
+    fn to_string(&self) -> String {
+        let Error(s) = self;
+        return s.clone();
     }
 }
 
-type Result<T> = result::Result<T, Error>;
+impl From<String> for Error {
+    fn from(s: String) -> Error {
+        Error(s)
+    }
+}
+
+impl From<&str> for Error {
+    fn from(s: &str) -> Error {
+        Error(String::from(s))
+    }
+}
+
+impl From<serialport::Error> for Error {
+    fn from(e: serialport::Error) -> Error {
+        Error(e.to_string())
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Error {
+        Error(e.to_string())
+    }
+}
+
+impl From<wire::response::ParseError> for Error {
+    fn from(e: wire::response::ParseError) -> Error {
+        Error(e.to_string())
+    }
+}
+
+pub type Result<T> = result::Result<T, Error>;
 
 pub struct T6615 {
     port: serialport::TTYPort,
@@ -36,8 +68,8 @@ impl T6615 {
     pub fn execute<S, T, E>(&mut self, s: S) -> Result<T>
     where
         S: Into<wire::Payload>,
-        T: TryFrom<wire::Payload, Error = E>,
         E: ToString,
+        T: TryFrom<wire::Payload, Error = E>,
     {
         // Convert the command into the wire message format.
         let out_p: wire::Payload = s.into();
@@ -78,6 +110,6 @@ impl T6615 {
         self.port.read_exact(&mut body)?;
 
         // And unmarshal the reply body into a reply type.
-        return Ok(T::try_from(wire::Payload(body))?);
+        return Ok(T::try_from(wire::Payload(body)).map_err(|e| e.to_string())?);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,14 @@ use tokio;
 use std::net;
 use prometheus;
 use prometheus::{Encoder};
+use std::env;
+use std::process;
+mod device;
+mod wire;
+use std::thread;
+use std::time;
 
-async fn serve_metrics() {
+async fn serve_metrics(addr: &str) {
     let reg = prometheus::default_registry();
 
     let routes = warp::path!("metrics").map(move || {
@@ -18,24 +24,54 @@ async fn serve_metrics() {
         return String::from_utf8(out).unwrap();
     });
 
-    let addr: net::IpAddr = "0.0.0.0".parse().unwrap();
+    println!("serving metrics on {}:8000", addr);
+    let addr: net::IpAddr = addr.parse().unwrap();
     warp::serve(routes).run((addr, 8000)).await;
 }
 
-async fn tick_counter() {
-    let ctr = prometheus::register_int_counter!(
-        "ticks", "Ticks up consistently").unwrap();
+async fn measure(mut sensor: device::T6615) {
+    let gague = prometheus::register_gauge!(
+        "co2_ppm", "The current concentration of CO2 in parts per million.")
+        .expect("unable to setup CO2 gauge");
+
+    // Update every interval.
     let mut every = tokio::time::interval(
-        tokio::time::Duration::from_millis(500));
+        tokio::time::Duration::from_secs(25));
     loop {
         every.tick().await;
-        ctr.inc();
+
+        println!("Measuring...");
+        let m: device::Result<wire::response::GasPPM> = sensor.execute(
+            wire::command::Read(wire::Variable::GasPPM));
+        match m {
+            Ok(v) => gague.set(v.concentration().ppm() as f64),
+            Err(e) => eprintln!("Error reading value: {}", e.to_string()),
+        }
     }
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Must supply serial device address.");
+        process::exit(1);
+    }
+    let mut sensor = device::T6615::new(&args[1])
+        .expect("unable to connect to sensor");
+
+    println!("waiting for warmup...");
+    loop {
+        let status: wire::response::Status = sensor.execute(
+            wire::command::Status).unwrap();
+        if !status.in_warmup() {
+            break;
+        }
+        thread::sleep(time::Duration::from_millis(500));
+    }
+
+    println!("Booting...");
     let mut rt = tokio::runtime::Runtime::new().unwrap();
-    rt.block_on(async {
-        tokio::join!(tick_counter(), serve_metrics());
+    rt.block_on(async move {
+        tokio::join!(measure(sensor), serve_metrics("0.0.0.0"));
     });
 }
