85bc9eaf401045d3fe43631641b48a4db1d5953c
Josh Kunz <josh@kunz.xyz>
2021-03-19 21:52:57 -0700

    Re-write co2 measure using hooks.
    Hooks simplify the background polling logic quite a bit.

--- frontend/src/co2_measure.tsx
+++ frontend/src/co2_measure.tsx
@@ -1,58 +1,55 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Badge from 'react-bootstrap/Badge';
 import axios from 'axios';
-import {CancelTokenSource as AxiosCancelTokenSource} from 'axios';
 
 const READING_POLLING_INTERVAL_MS = 1000;
 
-class Reading extends React.Component<{}, {last_reading?: number}> {
-  state = {last_reading: undefined};
-  poller?: any = undefined;
-  signal?: AxiosCancelTokenSource = undefined;
-
-  componentDidMount() {
-    this.signal = axios.CancelToken.source();
-    const token = this.signal.token;
-    this.poller = setInterval(async () => {
+function useCO2Poller(onReading: (reading: number) => void) {
+  const onlyOnUnmountFilter: any[] = [];
+  useEffect(() => {
+    const signal = axios.CancelToken.source();
+    const poller = setInterval(async () => {
       try {
-        const resp = await axios.get('/co2', {cancelToken: token});
+        const resp = await axios.get('/co2', {cancelToken: signal.token});
         const reading = resp.data;
-        this.setState({
-          last_reading: reading,
-        });
+        onReading(reading);
       } catch {
         // TODO(jkz): Properly handle actual fetch failures by notifying
         // the user.
       }
     }, READING_POLLING_INTERVAL_MS);
-  }
+    return () => {
+      signal.cancel();
+      clearInterval(poller);
+    };
+  }, onlyOnUnmountFilter);
+}
 
-  componentWillUnmount() {
-    this.signal?.cancel();
-    clearInterval(this.poller);
-    this.signal = undefined;
-    this.poller = undefined;
-  }
+function Reading() {
+  const [last_reading, setLastReading] = useState<number | undefined>(
+    undefined
+  );
+  useCO2Poller(r => {
+    setLastReading(r);
+  });
 
-  render() {
-    let concentration = <span>loading...</span>;
-    if (this.state.last_reading !== undefined) {
-      concentration = (
-        <>
-          <span className="co2_measurement">{this.state.last_reading}</span>
-          <span className="co2_units">ppm</span>
-        </>
-      );
-    }
-    return (
-      <div className="co2_box">
-        <Badge variant="dark">
-          CO<sub>2</sub>
-        </Badge>
-        {concentration}
-      </div>
+  let concentration = <span>loading...</span>;
+  if (last_reading !== undefined) {
+    concentration = (
+      <>
+        <span className="co2_measurement">{last_reading}</span>
+        <span className="co2_units">ppm</span>
+      </>
     );
   }
+  return (
+    <div className="co2_box">
+      <Badge variant="dark">
+        CO<sub>2</sub>
+      </Badge>
+      {concentration}
+    </div>
+  );
 }
 
 export {Reading};
